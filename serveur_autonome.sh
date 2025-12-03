cc5cf3c49d1565df76cff6863f3037efcf696a1d
N3k0ma77 <leruse54@gmail.com>
2025-12-03 16:00:19 +0100

    fonction de journalisation standardisée

--- serveur_autonome.sh
+++ serveur_autonome.sh
@@ -1,104 +1,19 @@
 #!/bin/bash
-Léo
+#Journalisation et robustesse (seb)
+
+LOG_DIR="/var/log/maintenance"
+LOG_FILE="$LOG_DIR/maintenance_$(date +\%Y\%m\%d_\%H\%M\%S).log"
+
+mkdir -p "$LOG_DIR"
+
+log_message() {
+    local LEVEL="$1" 
+    local MESSAGE="$2"
+    
+    LOG_LINE="[$(date +\%H:\%M:\%S)] [${LEVEL^^}] ${MESSAGE}"
+    
+    echo "$LOG_LINE" >> "$LOG_FILE"
+    
+    echo "$LOG_LINE"
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Partie de Léo
-toto
