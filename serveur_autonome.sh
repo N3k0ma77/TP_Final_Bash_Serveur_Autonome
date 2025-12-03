a7a8d4a13c9c6d025d63724b0331eee09cba0f9b
Keowayy <rorobertpetit@gmail.com>
2025-12-03 16:01:52 +0100

    Identification des users inactifs

--- serveur_autonome.sh
+++ serveur_autonome.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-Léo
 
+#Identification des users inactifs
+#90j d'inactivite
+J_INACTIF=90
+CHEMIN_HOME="/home"
 
+#Liste des users dans une liste
+FICHIER_TEMP="/tmp/liste_inactifs.txt" 
 
+#Debut analyse
+echo "=== DEBUT DE L'ANALYSE ==="
+echo "Recherche des repertoires inactifs depuis plus de $J_INACTIF jours..."
+echo "Le resultat est  stocké dans : $FICHIER_TEMP"
+
+#(https://www.ionos.fr/digitalguide/serveur/configuration/commande-find-sous-linux/ - https://www.christophelebot.fr/ressources/shell-bash-commandes-linux-utiles-efficaces/)
+find $CHEMIN_HOME -maxdepth -type d -atime +$J_INACTIF > $FICHIER_TEMP
+
+#Afficher la liste 
+echo ""
+echo "=== LISTE DES REPERTOIRES INACTIFS ==="
 
 
 
